4b56b1a9f7cb371dbb48683107b07372f03ba348
azatbek <sadykowazatbek@gmail.com>
2024-03-02 14:09:23 +0600

    Добавлены новые компоненты, Register - дает возможность зарегестрироваться на сайт. Login - если пользователь вышел, то может вновь восстановить данные. UserMenu - показывается только если пользователь зарегистрирован. Есть возможность выйти logout. Добавлен axiosApi для запросов на сервер. UsersSlice получает пользователя.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import AppToolbar from './components/UI/AppToolbar.tsx';
+import Login from './features/Users/Login.tsx';
+import Register from './features/Users/Register.tsx';
 
 function App() {
-
   return (
     <>
-
+      <header>
+        <AppToolbar />
+      </header>
+      <Routes>
+        <Route path="/" element={'main'} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
---
+++ frontend/src/types.d.ts
@@ -0,0 +1,40 @@
+export interface RegisterMutation {
+  username: string;
+  password: string;
+  displayName: string;
+  phone: string;
+}
+
+export interface LoginMutation {
+  username: string;
+  password: string;
+}
+
+export interface UserTypes {
+  _id: string;
+  username: string;
+  token: string;
+  displayName: string;
+  phone: string;
+}
+
+export interface ValidationError {
+  errors: {
+    [key: string]: {
+      name: string;
+      message: string;
+    },
+  },
+  message: string;
+  name: string;
+  _message: string;
+}
+
+export interface RegisterResponse {
+  message: string;
+  user: UserTypes;
+}
+
+export interface GlobalError {
+  error: string;
+}
